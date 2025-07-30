20c22b70a7bf8cc1576d6fca8827ba155d630297
juanrar <juanchilorenzo@gmail.com>
2025-07-30 19:49:26 -0300

    implementacion de funcionalidad de seguir/dejar de seguir

--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,6 +1,15 @@
 import './App.css'
+import { useState } from 'react'
 
-export function TwitterFollowCard({children, userName, isFollowing}) {
+export function TwitterFollowCard({children, userName, initialIsFollowing = false}) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
@@ -16,8 +25,9 @@ export function TwitterFollowCard({children, userName, isFollowing}) {
             </header>
 
             <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
                 </button>
             </aside>
         </article>
