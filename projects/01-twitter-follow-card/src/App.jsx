b15fa66693e69c3e0303af572f66ab2931f0f256
juanrar <juanchilorenzo@gmail.com>
2025-07-30 19:51:15 -0300

    Actualizacion del componente para que renderice dinamicamente los componentes de TwitterFollowCard

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,32 +1,52 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Fiti Sious',
+        isFollowing: false
+    },
+    {
+        userName: 'loremipsum',
+        name: 'Lorem Ipsum',
+        isFollowing: true
+    },
+    {
+        userName: 'elonmusk',
+        name: 'John Doe',
+        isFollowing: true
+    },
+    {
+        userName: 'janesmith',
+        name: 'Jane Smith',
+        isFollowing: true
+    },
+    {
+        userName: 'mockuser1',
+        name: 'Mock User',
+        isFollowing: false
+    }
+]
+
 export function App() {
     return (
         <section className="App">
-            <TwitterFollowCard 
-                isFollowing 
-                userName='midudev'>
-                    Miguel Angel Duran
-                </TwitterFollowCard>
-    
-            <TwitterFollowCard 
-                isFollowing={false}
-                userName='frantoledo'>
-                    Franco Toledo
-                </TwitterFollowCard>
+        {
+            users.map(user => {
+                const { userName, name, isFollowing } = user
+                return(
+                    <TwitterFollowCard
+                    key = {userName}
+                    userName = {userName}
+                    initialIsFollowing = {isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
 
-            <TwitterFollowCard 
-                isFollowing={false}
-                userName='juanar'>
-                    Juan Carlos lorenzo
-                </TwitterFollowCard>
-            
-            <TwitterFollowCard 
-                isFollowing 
-                userName='leomessi'>
-                    Messirve
-                </TwitterFollowCard>
+                )
+            })
+        }
         </section>
     )
 }
